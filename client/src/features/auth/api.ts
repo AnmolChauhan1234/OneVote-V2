6aa5f2e4ca4934f3e8264d26ecab4f2286057095
Anmol Rai <anmolrai0307@gmail.com>
2026-04-08 23:07:01 +0530

    removed only api.ts and schema.ts files and giving more structure to them

--- client/src/features/auth/api.ts
+++
@@ -1,9 +0,0 @@
-import apiCaller from '@/lib/apiCaller';
-import type { LoginCredentials, LoginResponse, RefreshResponse } from './types/types';
-
-export const authApi = {
-  loginUser: (credentials: LoginCredentials) => apiCaller.post<LoginResponse>('/auth/login/', credentials),
-  getMe: () => apiCaller.get('/auth/me/'),
-  logoutUser: () => apiCaller.post('/auth/logout/'),
-  refreshToken: () => apiCaller.post<RefreshResponse>('/auth/token/refresh/'),
-};
