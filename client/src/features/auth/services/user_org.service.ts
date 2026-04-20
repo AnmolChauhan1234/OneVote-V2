e7c8b60cf214ee4480510f46c313c2aad8819b7b
AnmolChauhan <acanmolchauhan1234@gmail.com>
2026-04-20 22:42:50 +0530

    refactor: update user organization service to use AUTH API URL constants

--- client/src/features/auth/services/user_org.service.ts
+++ client/src/features/auth/services/user_org.service.ts
@@ -7,22 +7,22 @@ import axiosClient from "@/lib/instances/axios";
 import { UserOrgIdentifierCreateFormData } from "../schemas/user_org.schema";
 
 export async function addUserOrgId(payload: UserOrgIdentifierCreateFormData): Promise<UserOrgIdentifierCreateFormData> {
-  const res = await axiosClient.post<UserOrgIdentifierResponse>(API_URLS.USER_ORG.ADD_ORG_IDS, payload);
+  const res = await axiosClient.post<UserOrgIdentifierResponse>(API_URLS.AUTH.ADD_ORG_IDENTIFIER, payload);
   return res.data;
 }
 
 
 export async function getUserOrgIds(): Promise<ListUserOrgIdentifiersResponse> {
-  const res = await axiosClient.get<ListUserOrgIdentifiersResponse>(API_URLS.USER_ORG.GET_ORG_IDS);
+  const res = await axiosClient.get<ListUserOrgIdentifiersResponse>(API_URLS.AUTH.GET_ORG_IDENTIFIER);
   return res.data;
 }
 
-export async function updateUserOrgIdentifier(identifier_id: string, payload: {identifier_value: string}): Promise<UserOrgIdentifierUpdateResponse> {
-  const res = await axiosClient.put<UserOrgIdentifierUpdateResponse>(API_URLS.USER_ORG.UPDATE_ORG_IDENTIFIER(identifier_id), payload);
+export async function updateUserOrgIdentifier(identifier_id: string, payload: { identifier_value: string }): Promise<UserOrgIdentifierUpdateResponse> {
+  const res = await axiosClient.put<UserOrgIdentifierUpdateResponse>(API_URLS.AUTH.UPDATE_ORG_IDENTIFIER(identifier_id), payload);
   return res.data;
 }
 
 export async function deleteUserOrgIdentifier(identifier_id: string): Promise<UserOrgIdentifierDeleteResponse> {
-  const res = await axiosClient.delete<UserOrgIdentifierDeleteResponse>(API_URLS.USER_ORG.DELETE_ORG_IDENTIFIER(identifier_id));
+  const res = await axiosClient.delete<UserOrgIdentifierDeleteResponse>(API_URLS.AUTH.DELETE_ORG_IDENTIFIER(identifier_id));
   return res.data;
 }
